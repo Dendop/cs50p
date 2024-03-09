5b5055053a5979b1119ef094dba599046da8a0e1
Dendop <denopod@gmail.com>
2024-03-09 16:13:31 +0000

    Update ari_formater.py

--- ari_formater.py
+++ ari_formater.py
@@ -1,35 +1,30 @@
 def arithmetic_arranger(problems, show_answers=False):
     arranged_problems = ""
-    
-    # Lists to store the lengths of each number in the problem
-    first_number_lengths = []
-    second_number_lengths = []
-    
-    # Loop through each problem to collect lengths of numbers
     for each in problems:
         split_each = each.split()
-        first_number_lengths.append(len(split_each[0]))
-        second_number_lengths.append(len(split_each[2]))
+        first_number = split_each[0]
+        operator = split_each[1]
+        second_number = split_each[2]
         
-        # Construct the formatted strings for the first number
-        arranged_problems += f"{split_each[0]:>{len(split_each[2])}}    "
+        # Calculate the length of the longest number (either first_number or second_number)
+        max_length = max(len(first_number), len(second_number))
+        
+        # Construct the formatted strings
+        arranged_problems += f"{first_number:>{max_length + 1}}"
+        arranged_problems += "    "  # Add 4 spaces to separate each set
+        
     arranged_problems += "\n"  # Add a newline to move to the next line
     
-    # Loop through each problem to construct formatted strings for the operator and second number
     for each in problems:
         split_each = each.split()
+        first_number = split_each[0]
+        operator = split_each[1]
+        second_number = split_each[2]
         
-        # Construct the formatted strings for the operator and second number
-        arranged_problems += f"{split_each[1]} {split_each[2]:>{len(split_each[0])}}    "
-    
-    arranged_problems += "\n"  # Add a newline before the line of dashes
-    
-    # Calculate the maximum length of all numbers in the problems
-    max_length_all = max(max(first_number_lengths), max(second_number_lengths))
-    
-    # Construct the line of dashes
-    arranged_problems += "-" * (max_length_all + 2)  # Add a line of dashes
-    
+        # Construct the formatted strings for operator and second number
+        arranged_problems += f"{operator} {second_number:>{len(second_number)}}"
+        arranged_problems += "    "  # Add 4 spaces to separate each set
+        
     print(arranged_problems)
     return problems
 
