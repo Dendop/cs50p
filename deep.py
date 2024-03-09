0f9a6c17441db8aa9ede8e8296f9546804a155a4
Dendop <denopod@gmail.com>
2024-03-09 11:52:09 +0000

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,11 @@
+def main():
+    print('What is the Answer to the Great Question of Life, the Universe, and Everything?')
+    answer = input('Your answer: ')
+
+    if answer == "42" or answer == "forty two" :
+        print('Yes')
+    else:
+        print('No')
+    
+if __name__ == "__main__":
+    main()
